ca1c8025285b16bbcf370c4f3e3a7c3a49f3220f
PoisonGold333 <alvaro.dorado91@gmail.com>
2025-06-17 18:28:46 -0500

    Server Express, API, mongoDB connection, .env

---
+++ api.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var router = express.Router();
+//var query = "mongodb+srv://alvaroDorado:admin123@cluster0.9pym6el.mongodb.net/taskBD?retryWrites=true&w=majority&appName=Cluster0"
+const db = (query);
+
+let environment = null;
+
+if (!process.env.ON_PRODUCTION) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(query, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conexión a la base de datos exitosa');
+}).catch((err) => {
+    console.error('Error al conectar a la base de datos', err);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+const api = require('./api');
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+});
+
+app.use('/api', api);
